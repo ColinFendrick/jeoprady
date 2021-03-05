a45f858e2d501ecb52b35c92686194eecf6e0596
Colin Fendrick <colin.fendrick@gmail.com>
2021-03-05 08:22:49 -0800

    Cleanup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,9 @@
+const router = require('express').Router();
+
 const { verifySignUp } = require('../middleware');
 const auth = require('../controllers/auth.controller');
 
 const authRoutes = app => {
-
-  const router = require('express').Router();
-
   router.use((req, res, next) => {
     res.header(
       'Access-Control-Allow-Headers',
